b1e35c39e902e32eb7c6397dacd033e310db17bc
Barbara Step <b.barbarastep@gmail.com>
2026-08-19 13:38:48 +0300

    Implement Anki core classes

---
+++ src/anki/anki.py
@@ -0,0 +1,39 @@
+import copy
+
+
+class Anki:
+    """Класс приложения для тренировки слов."""
+
+    def __init__(self, *, words: dict[str, str] | None = None) -> None:
+        if words is None:
+            words = {}
+
+        if not isinstance(words, dict):
+            raise ValueError("words должен быть словарём")
+
+        self._words: dict[str, str] = {}
+
+        for word, translation in words.items():
+            self.add_word(word, translation)
+
+    def get_words(self) -> dict[str, str]:
+        """Возвращает копию словаря слов."""
+        return copy.copy(self._words)
+
+    @staticmethod
+    def normalize_word(word: str) -> str:
+        if not isinstance(word, str):
+            raise ValueError("word должно быть строкой")
+
+        return word.strip().lower()
+
+    def add_word(self, word: str, translation: str) -> None:
+        """Добавляет слово и перевод в словарь."""
+
+        if not isinstance(word, str) or not isinstance(translation, str):
+            raise ValueError("word и translation должны быть строками")
+
+        normalized_word = self.normalize_word(word)
+        normalized_translation = self.normalize_word(translation)
+
+        self._words[normalized_word] = normalized_translation
---
+++ src/anki/loader.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from typing import Dict, Union
+
+
+class TextFileLoader:
+    """Класс загрузки слов из текстового файла."""
+
+    def __init__(self, *, file_path: Union[str, Path]
+                 = "./words.txt") -> None:
+        path = Path(file_path)
+
+        if path.is_dir():
+            raise ValueError(
+                "file_path должен указывать на файл, "
+                "а не на директорию"
+            )
+
+        self._file_path = path
+
+    def load_words(self) -> Dict[str, str]:
+        """Загружает словарь слов из текстового файла."""
+        words = {}
+
+        if not self._file_path.exists():
+            return {}
+
+        with open(self._file_path, "r", encoding="utf-8") as file:
+            for line in file:
+                line = line.strip()
+
+                if line.count(",") == 1:
+                    word, translation = line.split(",", 1)
+                    word = word.strip()
+                    translation = translation.strip()
+                    words[word] = translation
+
+        return words
+
+    def save_words(self, words: Dict[str, str]) -> None:
+        """Сохраняет словарь слов в текстовый файл."""
+        if not isinstance(words, dict):
+            raise ValueError("words должен быть словарём")
+
+        file_path = self._file_path
+
+        with open(file_path, "w", encoding="utf-8") as file:
+            for word, translation in words.items():
+                file.write(f"{word},{translation}\n")
